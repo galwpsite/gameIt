228eec20dd218fc95c5d39ac54874189732e7628
galwpsite <gal@wpsite.co.il>
2015-09-10 01:48:34 +0300

    First stage done

--- gameIt.py
+++ gameIt.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 h=helper.Helper()
 @app.route('/')
 def main():
+    #delete later
     return render_template('main.html',numOfGames=h.getNumOfGames(), numOfUsers=h.getNumOfUsers())
 
 
