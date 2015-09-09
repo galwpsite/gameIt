692f83b64cbb64c00c87e04df73e5919aae94fce
galwpsite <gal@wpsite.co.il>
2015-09-10 01:45:53 +0300

    First stage done

---
+++ gameIt.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template, request, session, flash, redirect, url_for, g
+from functools import wraps
+from flask import g, request, redirect, url_for
+import pyodbc
+import helper
+app = Flask(__name__)
+h=helper.Helper()
+@app.route('/')
+def main():
+    return render_template('main.html',numOfGames=h.getNumOfGames(), numOfUsers=h.getNumOfUsers())
+
+
+
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
---
+++ helper.py
@@ -0,0 +1,24 @@
+__author__ = 'galwp'
+import pyodbc
+class Helper():
+    #Settings:
+    connection_str ="""
+    Driver={SQL Server Native Client 11.0};
+    Server=GAL-PC\SQLEXPRESS;
+    Database=gameIt_DBSummer;
+    Trusted_Connection=yes;
+    """
+    __db_connection = pyodbc.connect(connection_str)
+    __db_connection.autocommit = True
+    __db_cursor = __db_connection.cursor()
+
+    def getAllGames(self):
+        return self.__db_cursor.execute("EXECUTE getAllGames;").fetchall()
+    def getAllUsers(self):
+        return self.__db_cursor.execute("EXECUTE getAllUsers;").fetchall()
+
+    def getNumOfGames(self):
+          return len(list(self.getAllGames()))
+
+    def getNumOfUsers(self):
+      return len(list(self.getAllUsers()))
