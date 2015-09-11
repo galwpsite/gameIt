3317a9695da9f4d57afcb813854595bde82db321
galwpsite <gal@wpsite.co.il>
2015-09-11 06:01:54 +0300

    added validton to forms and "add new game" button

--- gameIt.py
+++ gameIt.py
@@ -25,7 +25,7 @@ def main():
 
 
 @app.route('/home/games',methods=['GET','POST'])
-@flask_breadcrumbs.register_breadcrumb(app,'.Games','Games')
+@flask_breadcrumbs.register_breadcrumb(app,'.Games','Games List')
 def games():
     if request.method == 'POST':
         form=request.form
@@ -40,14 +40,18 @@ def gamesSearch():
     posts = [dict(id=row[0],name=row[1],desc=row[2]) for row in h.findGameByName(form['name']) ]
     return render_template('archive-game.html',pageTitle="Manage Games",pageSubTitle="List Of All The Games: ",posts=posts)
 
-@app.route('/home/games/edit')
+@app.route('/home/games/edit',methods=['GET','POST'])
 @flask_breadcrumbs.register_breadcrumb(app,'.Games.Edit','Edit Game')
 def gameEdit():
+    if request.method == 'POST':
+            form=request.form
+            if 'search-form' in form:
+                return redirect(url_for("gamesSearch"),code=307)
     gameNo = request.args.get('gameNo')
     game= [dict(id=row[0],name=row[1],desc=row[2]) for row in h.findGameByGameNo(gameNo)]
     gameName = (game[0].get('name'))
     gameDesc = (game[0].get('desc'))
-    return render_template('edit-game.html',pageTitle="Manage Games",pageSubTitle="Edit Game: ",gameNo=gameNo,gameName=gameName,gameDesc=gameDesc)
+    return render_template('edit-game.html',pageTitle="Manage Games",pageSubTitle="Edit Game: "+gameName,gameNo=gameNo,gameName=gameName,gameDesc=gameDesc)
 
 
 
